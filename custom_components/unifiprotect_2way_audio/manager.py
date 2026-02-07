a63c36f6a32280247326535c5bb8c6b75b9989e4
Teagan Glenn <that@teagantotally.rocks>
2026-02-07 15:28:02 -0700

    Pass media player id to microphone

--- custom_components/unifiprotect_2way_audio/manager.py
+++ custom_components/unifiprotect_2way_audio/manager.py
@@ -87,6 +87,7 @@ class StreamConfigManager:
                 camera_entity.entity_id,
                 camera_entity.unique_id,
                 device_info,
+                None if len(media_player_entities) == 0 else media_player_entities[0].entity_id
             )
             _LOGGER.debug(
                 "Created microphone entity for camera: %s",
